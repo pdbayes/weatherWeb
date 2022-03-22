79aeffcf650dffc23bb22b7f9bfc654c4e34921c
pdbayes <58221502+pdbayes@users.noreply.github.com>
2022-03-22 16:55:40 +0000

    more updates

--- demo.js
+++ demo.js
@@ -115,17 +115,17 @@ function getData(path) {
         })
         .then(function (data) {
             var data_a = data.map(function (e) {
-                if(path === 'rain'){
-                return [Date.parse(e.time), e.sum];
+                if (path === 'rain') {
+                    return [Date.parse(e.time), e.sum];
                 }
-                else{
+                else {
                     return [Date.parse(e.time), e.mean];
                 }
             });
 
             //var current = data_a[data_a.len - 1][1];
             // console.log('current' + current);
-            console.log(data_a[data_a.length-1][1]);
+            console.log(data_a.length + ' ' + data_a[data_a.length - 1][1]);
             var meas_name = '';
             if (path === 'temp') {
                 var meas_name = 'Temperature';
@@ -134,10 +134,15 @@ function getData(path) {
                 var maxScale = 30;
                 var chartType = 'spline';
                 var stopCols = [
-                    [0.00, 'red'],
-                    [0.3, 'orange'],
-                    [0.5, 'lightblue'],
-                    [1.00, 'Blue']
+                    [0, 'rgb(213, 62, 79)'],
+                    [0.1, 'rgb(244, 109, 67)'],
+                    [0.15, 'rgb(253,174,97)'],
+                    [0.2, 'rgb(254,224,139)'],
+                    [0.25, 'rgb(255, 255, 191)'],
+                    [0.3, 'rgb(230, 245, 152)'],
+                    [0.4, 'rgb(171, 221, 164)'],
+                    [0.45, 'rgb(102, 194, 165)'],
+                    [1, 'rgb(50, 136, 189)']
                 ];
             }
             else if (path === 'humidity') {
@@ -160,7 +165,7 @@ function getData(path) {
                 var stopCols = [
                     [0.00, 'rgba(0,255,0,0.5)'],
                     [0.4, 'rgba(252,232,3,0.5)'],
-                    [0.6, 'rgba(252,232,3,0.5'],
+                    [0.45, 'rgba(252,232,3,0.5'],
                     [1.00, 'rgba(255,0,0,0.5)']
                 ]
             }
@@ -182,10 +187,42 @@ function getData(path) {
 
             Highcharts.chart(div_name, {
                 chart: {
+                    events: {
+                        load: function () {
+                            var chart = this;
+                            var extremes = chart.plotBox.y;
+                            var yMin = chart.plotBox.y;
+                            var yMax = chart.plotBox.y + chart.plotBox.height;
+                            console.log('yAxis max/min' + yMax + '/' + yMin);
+                            console.log(chart.plotBox.y);
+                            console.log(chart.plotBox.height);
+                            chart.series[0].update({
+                                color: {
+                                    //linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+                                    linearGradient: [0, yMin, 0, yMax],
+                                    stops: stopCols
+                                }
+                            })
+                        },
+                        update: function () {
+                            var chart = this;
+                            var extremes = chart.yAxis[0].getExtremes();
+                            var yMax = chart.yAxis[0].toPixels(extremes.max);
+                            var yMin = chart.yAxis[0].toPixels(extremes.min);
+                            console.log('yAxis max/min' + yMax + '/' + yMin);
+                            chart.series[0].color = {
+                                //linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
+                                linearGradient: [0, yMin, 0, yMax],
+                                stops: stopCols
+                            }
+                        }
+                    },
                     type: chartType,
                     borderWidth: 3
 
                 },
+
+                margin: [0, 0, 0, 0],
 
                 title: {
                     text: meas_name
@@ -226,7 +263,7 @@ function getData(path) {
                     data: data_a,
                     color: {
                         //linearGradient: { x1: 0, x2: 0, y1: 0, y2: 1 },
-                        linearGradient: [ 0, 0, 0, 295],
+                        linearGradient: [0, 0, 0, 0],
                         stops: stopCols
                     }
                 }]
