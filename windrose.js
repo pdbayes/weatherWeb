53f64484595c247a28bc10104be20feb6a10c325
pdbayes <58221502+pdbayes@users.noreply.github.com>
2022-04-03 18:37:09 +0100

    new forecast page added

--- windrose.js
+++ windrose.js
@@ -57,6 +57,116 @@ function windrose() {
             var j = dir_arr(71, 100);
 
             var chartBench = Highcharts.chart('windrose', {
+                exporting: {
+                    buttons: {
+                      contextButton: {
+                        menuItems: [{
+                          text: 'Average',
+                          onclick: function() {
+                                                //chartBench.series[0].setData([110, 100, 120, 130]);
+                    var a = dir_arr(0, 2);
+                    //console.log('a = ' + a);
+                    var b = dir_arr(3, 5);
+                    //console.log('b = ' + b);
+                    var c = dir_arr(6, 10);
+                    //console.log('c = ' + c);
+                    var d = dir_arr(11, 20);
+                    // console.log('d = ' + d);
+                    var e = dir_arr(21, 30);
+                    //console.log('e = ' + e);
+                    var f = dir_arr(31, 40);
+                    var g = dir_arr(41, 50);
+                    var h = dir_arr(51, 60);
+                    var i = dir_arr(61, 70);
+                    var j = dir_arr(71, 100);
+                    chartBench.series[0].update({
+                        data: a
+                    });
+                    chartBench.series[1].update({
+                        data: b
+                    });
+                    chartBench.series[2].update({
+                        data: c
+                    });
+                    chartBench.series[3].update({
+                        data: d
+                    });
+                    chartBench.series[4].update({
+                        data: e
+                    });
+                    chartBench.series[5].update({
+                        data: f
+                    });
+                    chartBench.series[6].update({
+                        data: g
+                    });
+                    chartBench.series[7].update({
+                        data: h
+                    });
+                    chartBench.series[8].update({
+                        data: i
+                    });
+                    chartBench.series[9].update({
+                        data: j
+                    });
+
+                    chartBench.redraw();
+                          }
+                        }, {
+                          text: 'Gusts',
+                          onclick: function() {
+                            var a = dir_arr(0, 2, 'gust');
+                            //console.log('a = ' + a);
+                            var b = dir_arr(3, 5, 'gust');
+                            //console.log('b = ' + b);
+                            var c = dir_arr(6, 10, 'gust');
+                            //console.log('c = ' + c);
+                            var d = dir_arr(11, 20, 'gust');
+                            // console.log('d = ' + d);
+                            var e = dir_arr(21, 30, 'gust');
+                            //console.log('e = ' + e);
+                            var f = dir_arr(31, 40, 'gust');
+                            var g = dir_arr(41, 50, 'gust') ;
+                            var h = dir_arr(51, 60, 'gust');
+                            var i = dir_arr(61, 70, 'gust');
+                            var j = dir_arr(71, 100, 'gust');
+        
+                            chartBench.series[0].update({
+                                data: a
+                            });
+                            chartBench.series[1].update({
+                                data: b
+                            });
+                            chartBench.series[2].update({
+                                data: c
+                            });
+                            chartBench.series[3].update({
+                                data: d
+                            });
+                            chartBench.series[4].update({
+                                data: e
+                            });
+                            chartBench.series[5].update({
+                                data: f
+                            });
+                            chartBench.series[6].update({
+                                data: g
+                            });
+                            chartBench.series[7].update({
+                                data: h
+                            });
+                            chartBench.series[8].update({
+                                data: i
+                            });
+                            chartBench.series[9].update({
+                                data: j
+                            });
+                            chartBench.redraw(); // redraw only after add all series
+                          }
+                        }]
+                      }
+                    }
+                  },
                 series: [
                     {
                         name: '0-2 mph',
@@ -107,11 +217,23 @@ function windrose() {
                 },
 
                 title: {
-                    text: 'Wind rose'
+                    text: 'Wind rose',
+                    style: {
+                        color: '#fff',
+                        fontWeight: 'bold',
+
+                    }
                 },
+                subtitle: {
+                    text: 'Change between ave and gusts using the menu',
+                    style: {
+                        color: '#fff'
+
+                    }
+                } ,
 
                 pane: {
-                    size: '85%'
+                    size: '90%'
                 },
 
                 legend: {
@@ -157,114 +279,6 @@ function windrose() {
 
 
 
-            $("#list").on('change', function () {
-                //alert('f')
-                var selVal = $("#list").val();
 
-                if (selVal == "Mean") {
-                    //chartBench.series[0].setData([110, 100, 120, 130]);
-                    var a = dir_arr(0, 2);
-                    //console.log('a = ' + a);
-                    var b = dir_arr(3, 5);
-                    //console.log('b = ' + b);
-                    var c = dir_arr(6, 10);
-                    //console.log('c = ' + c);
-                    var d = dir_arr(11, 20);
-                    // console.log('d = ' + d);
-                    var e = dir_arr(21, 30);
-                    //console.log('e = ' + e);
-                    var f = dir_arr(31, 40);
-                    var g = dir_arr(41, 50);
-                    var h = dir_arr(51, 60);
-                    var i = dir_arr(61, 70);
-                    var j = dir_arr(71, 100);
-                    chartBench.series[0].update({
-                        data: a
-                    });
-                    chartBench.series[1].update({
-                        data: b
-                    });
-                    chartBench.series[2].update({
-                        data: c
-                    });
-                    chartBench.series[3].update({
-                        data: d
-                    });
-                    chartBench.series[4].update({
-                        data: e
-                    });
-                    chartBench.series[5].update({
-                        data: f
-                    });
-                    chartBench.series[6].update({
-                        data: g
-                    });
-                    chartBench.series[7].update({
-                        data: h
-                    });
-                    chartBench.series[8].update({
-                        data: i
-                    });
-                    chartBench.series[9].update({
-                        data: j
-                    });
-
-                    chartBench.redraw();
-                }
-                else if (selVal == "Gust") {
-
-                    var a = dir_arr(0, 2, 'gust');
-                    //console.log('a = ' + a);
-                    var b = dir_arr(3, 5, 'gust');
-                    //console.log('b = ' + b);
-                    var c = dir_arr(6, 10, 'gust');
-                    //console.log('c = ' + c);
-                    var d = dir_arr(11, 20, 'gust');
-                    // console.log('d = ' + d);
-                    var e = dir_arr(21, 30, 'gust');
-                    //console.log('e = ' + e);
-                    var f = dir_arr(31, 40, 'gust');
-                    var g = dir_arr(41, 50, 'gust') ;
-                    var h = dir_arr(51, 60, 'gust');
-                    var i = dir_arr(61, 70, 'gust');
-                    var j = dir_arr(71, 100, 'gust');
-
-                    chartBench.series[0].update({
-                        data: a
-                    });
-                    chartBench.series[1].update({
-                        data: b
-                    });
-                    chartBench.series[2].update({
-                        data: c
-                    });
-                    chartBench.series[3].update({
-                        data: d
-                    });
-                    chartBench.series[4].update({
-                        data: e
-                    });
-                    chartBench.series[5].update({
-                        data: f
-                    });
-                    chartBench.series[6].update({
-                        data: g
-                    });
-                    chartBench.series[7].update({
-                        data: h
-                    });
-                    chartBench.series[8].update({
-                        data: i
-                    });
-                    chartBench.series[9].update({
-                        data: j
-                    });
-                    chartBench.redraw(); // redraw only after add all series
-
-                }
-                else {
-
-                }
-            });
         });
 }
