745c7e56677290f00b518fc57b2594d8ebff00cc
pdbayes <58221502+pdbayes@users.noreply.github.com>
2022-04-21 14:12:52 +0100

    baro stuff

--- js/bar.js
+++ js/bar.js
@@ -1,4 +1,22 @@
+function windowWidth() {
+    if (window.innerWidth < 500) {
+        var vpw = window.innerWidth;
+    }
+    else {
+        var vpw = 500;
+    }
+    console.log('resizing ' + vpw);
+
+    return vpw;
+};
+
+
 $(function () {
+
+
+    var vpWidth = windowWidth();
+
+    
     $('#container').highcharts(
         {
             chart: {
@@ -6,8 +24,8 @@ $(function () {
                 //plotBackgroundImage: 'images/baro.png',
                 plotBorderWidth: 0,
                 plotShadow: false,
-                height: 500,
-                width: 500,
+                height: vpWidth,
+                width: vpWidth,
                 spacingTop: 15,
                 spacingRight: 15,
                 spacingBottom: 15,
@@ -38,7 +56,7 @@ $(function () {
                 background: null
             }
             ],
-            
+
 
             // the value axis
             yAxis: [
@@ -78,7 +96,7 @@ $(function () {
                     lineColor: '#000',
                     min: 0,
                     max: 100,
-                    lineWidth:0,
+                    lineWidth: 0,
                     minorTickWidth: 1,
                     minorTickLength: 3,
                     minorTickInterval: 5,
@@ -90,7 +108,7 @@ $(function () {
                     tickPosition: 'outside',
                     tickColor: '#000',
                     pane: 1
-                  }, {
+                }, {
                     labels: {
                         enabled: true,
                         distance: 15
@@ -100,19 +118,19 @@ $(function () {
                     },
                     min: -10,
                     max: 30,
-                    lineWidth:0,
+                    lineWidth: 0,
                     minorTickWidth: 1,
                     minorTickLength: 3,
-                    minorTickInterval: 5,
+                    minorTickInterval: 1,
                     tickWidth: 2,
                     tickLength: 5,
-                    tickInterval: 10,
+                    tickInterval: 5,
                     minorTickPosition: 'outside',
                     minorTickColor: '#000',
                     tickPosition: 'outside',
                     tickColor: '#000',
                     pane: 2
-                  }
+                }
 
             ],
 
@@ -145,96 +163,89 @@ $(function () {
                     },
                     yAxis: 1,
                     data: [0]
-                  }, {
+                }, {
                     animation: false,
                     dataLabels: {
                         enabled: false
                     },
                     yAxis: 2,
                     data: [-25]
-                  }
+                }
 
             ],
         },
 
         // Add some life
+
+
+
         function (chart) {
             if (!chart.renderer.forExport) {
-                //var diffW = chart.chartWidth - chart.plotWidth;
-                //var diffH = chart.chartHeight - chart.plotHeight;
 
-                var img = new Image();   // Create new img element
-                img.onload = function () {
-                    console.log('loading image');
-                    chart.renderer.image('images/newbaro.png', chart.plotLeft, chart.plotTop, chart.plotWidth, chart.plotHeight)
-                        .add();
-                }
-                img.src = 'images/newbaro.png'; // Set source path
-                setInterval(function () {
-                    //var point = chart.series[0].points[0];
-                    //var newVal;
-                    //var inc = Math.round((Math.random() - 0.5) * 60);
-                    // newVal = point.y + inc;
-                    // if (newVal < 950 || newVal > 1050) {
-                    //     newVal = point.y - inc;
-                    // }
-                    Promise.all([
-                        fetch('https://weathernode.tregrillfarmcottages.co.uk/pressure/current',{
-                            credentials: "include",
-                            credentials: 'same-origin'
-                        }),
-
-                        fetch('https://weathernode.tregrillfarmcottages.co.uk/temp/current',{
-                            credentials: "include",
-                            credentials: 'same-origin'
-                        }),
-                        
-                        fetch('https://weathernode.tregrillfarmcottages.co.uk/humidity/current', {
-                            credentials: "include",
-                            credentials: 'same-origin'
-                        })
-                    ]).then(function (responses) {
-                        // Get a JSON object from each of the responses
-                        return Promise.all(responses.map(function (response) {
-                            return response.json();
-                        }));
-                    })
-                        .then(function (data) {
-                            console.log(data);
-
-                            var speed = data[0].map(function (e) {
-                                return e.pressureHpa;
+                        function refresh(){
+                            
+                            Promise.all([
+                                fetch('https://weathernode.tregrillfarmcottages.co.uk/pressure/current', {
+                                    credentials: "include",
+                                    credentials: 'same-origin'
+                                }),
+        
+                                fetch('https://weathernode.tregrillfarmcottages.co.uk/temp/current', {
+                                    credentials: "include",
+                                    credentials: 'same-origin'
+                                }),
+        
+                                fetch('https://weathernode.tregrillfarmcottages.co.uk/humidity/current', {
+                                    credentials: "include",
+                                    credentials: 'same-origin'
+                                })
+                            ]).then(function (responses) {
+                                // Get a JSON object from each of the responses
+                                return Promise.all(responses.map(function (response) {
+                                    return response.json();
+                                }));
                             })
-                            var speed_point = chart.series[0].points[0];
-                            var speedVal;
-                            speedVal = speed;
-                            console.log(speedVal);
-                            speed_point.update(speedVal);
-
-                            var temp = data[1].map(function (e) {
-                                return  e.temperatureInC;
-                            })
-                            var temp_point = chart.series[2].points[0];
-                            var tempVal;
-                            tempVal = temp;
-                            console.log(tempVal);
-                            temp_point.update(tempVal);
-
-                            var hum = data[2].map(function (e) {
-                                return  e.humidityPercentage;
-                            })
-                            var hum_point = chart.series[1].points[0];
-                            var humVal;
-                            humVal = hum;
-                            console.log(humVal);
-                            hum_point.update(humVal);
-
-
-                            return chart;
-                        })
-                    //console.log(newVal);
-                    //point.update(newVal);
-                }, 7000);
+                                .then(function (data) {
+                                    if(typeof img != "undefined"){
+                                        img.destroy()}
+                                    var img = chart.renderer.image('images/newbaro.png', chart.plotLeft, chart.plotTop, chart.plotWidth, chart.plotHeight).add();
+        
+                                    //var img = chart.renderer.image('images/newbaro.png', chart.plotLeft, chart.plotTop, chart.plotWidth, chart.plotHeight).add();
+                                    var speed = data[0].map(function (e) {
+                                        return e.pressureHpa;
+                                    })
+                                    var speed_point = chart.series[0].points[0];
+                                    var speedVal;
+                                    speedVal = speed;
+                                    console.log(speedVal);
+                                    speed_point.update(speedVal);
+        
+                                    var temp = data[1].map(function (e) {
+                                        return e.temperatureInC;
+                                    })
+                                    var temp_point = chart.series[2].points[0];
+                                    var tempVal;
+                                    tempVal = temp;
+                                    console.log(tempVal);
+                                    temp_point.update(tempVal);
+        
+                                    var hum = data[2].map(function (e) {
+                                        return e.humidityPercentage;
+                                    })
+                                    var hum_point = chart.series[1].points[0];
+                                    var humVal;
+                                    humVal = hum;
+                                    console.log(humVal);
+                                    hum_point.update(humVal);
+                                    chart.setSize(windowWidth(), windowWidth(), doAnimation= true);
+                                    img.destroy();
+                                    var img = chart.renderer.image('images/newbaro.png', chart.plotLeft, chart.plotTop, chart.plotWidth, chart.plotHeight).add();
+        
+                                    return chart;
+                                })
+                        }
+                        refresh();
+                        setInterval(refresh, 7000);
             }
         }
 
