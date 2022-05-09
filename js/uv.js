be89dd62337262f9dc543135e7185d391739dc1d
pdbayes <58221502+pdbayes@users.noreply.github.com>
2022-05-09 15:36:47 +0100

    more fiddling

--- js/uv.js
+++ js/uv.js
@@ -9,13 +9,14 @@ function getUVIndex() {
         url: 'https://api.openuv.io/api/v1/uv?lat=50.443530&lng=-4.420760&alt=100',
         success: function (response) {
             var uvTime = new Date(response.result.uv_max_time);
-
+            var safeExp = response.result.safe_exposure_time.st3 + ' mins';
             var uvHours =uvTime.getUTCHours() + 1;
             var uvMins = uvTime.getUTCMinutes();
             var uvMaxTime = uvHours + ':' + uvMins;
             var uvDisplay = response.result.uv_max + ' @ ' + uvMaxTime;
             console.log(response.result.uv_max + ' @ ' + uvMaxTime)
             $('#uvDat').html(uvDisplay);
+            $('#expDat').html(safeExp);
         },
         error: function (response) {
             // handle error response
