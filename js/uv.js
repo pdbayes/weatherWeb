928b392ebbe6109973ca7ed681956d4b073f6058
pdbayes <58221502+pdbayes@users.noreply.github.com>
2022-05-10 13:05:57 +0100

    sun times

--- js/uv.js
+++ js/uv.js
@@ -9,7 +9,12 @@ function getUVIndex() {
         url: 'https://api.openuv.io/api/v1/uv?lat=50.443530&lng=-4.420760&alt=100',
         success: function (response) {
             var uvTime = new Date(response.result.uv_max_time);
-            var safeExp = response.result.safe_exposure_time.st3 + ' mins';
+            var sunrise = new Date(response.result.sun_info.sun_times.sunrise);
+            var sunset = new Date(response.result.sun_info.sun_times.sunset);
+            var sunriseTime = sunrise.getUTCHours() +1 + ":" + sunrise.getUTCMinutes();
+            var sunsetTime = sunset.getUTCHours() +1 + ":" + sunset.getUTCMinutes();
+            var sunUpDown = sunriseTime + " / " + sunsetTime;
+            var safeExp = (200 * 4)/(3 * response.result.uv_max).toFixed(0) + ' mins';
             var uvHours =uvTime.getUTCHours() + 1;
             var uvMins = uvTime.getUTCMinutes();
             var uvMaxTime = uvHours + ':' + uvMins;
@@ -17,6 +22,7 @@ function getUVIndex() {
             console.log(response.result.uv_max + ' @ ' + uvMaxTime)
             $('#uvDat').html(uvDisplay);
             $('#expDat').html(safeExp);
+            $('#sunupdown').html(sunUpDown);
         },
         error: function (response) {
             // handle error response
