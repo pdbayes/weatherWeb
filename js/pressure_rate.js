1296d0ff26fca3bb43201a9988bb8dfdd978f976
pdbayes <58221502+pdbayes@users.noreply.github.com>
2023-05-19 20:23:48 +0100

    zeyj

--- js/pressure_rate.js
+++ js/pressure_rate.js
@@ -132,7 +132,7 @@ function pressure_rate() {
         .then((resp) => resp.json())
         .then((data) => {
           const speed = data.map((e) => e.pressure);
-          console.log('difference: ' = speed.last - speed.first)
+          console.log('difference: ' + speed.last - speed.first)
           const speed_point = chart.series[0].points[0];
           let speedVal;
           speedVal = speed;
